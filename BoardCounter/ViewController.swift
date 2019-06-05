f795838b9d92f0719934e37134e893faa72c6750
Preston Haynes <prestonhaynes@me.com>
2019-06-05 10:40:08 -0600

    Initial Commit

---
+++ BoardCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoardCounter
+//
+//  Created by Preston on 6/5/19.
+//  Copyright © 2019 Daypark Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
