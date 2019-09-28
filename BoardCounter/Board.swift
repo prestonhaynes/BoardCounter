0c85ff564a83524a7f7767b536db929b2efb016f
Preston Haynes <prestonhaynes@me.com>
2019-09-27 20:57:04 -0600

    updated files

--- BoardCounter/Board.swift
+++ BoardCounter/Board.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+import CoreData
+
+
+class Board {
+	// Fields
+	
+	var whiteBoard: Double
+	var yellowBoard: Double
+	var orangeBoard: Double
+	var greenBoard: Double
+	var blueBoard: Double
+	var	brownBoard: Double
+	var blackBoard: Double
+	var date: Date
+	var tool: String
+	
+	// List of Tools
+	final var tools = ["Footsword", "Knifehand", "Ball of Foot", "Reverse Knifehand", "Forefist", "Backfist", "Heel"]
+	
+	// Board values
+	final var _bWhite = 0.25
+	final var _bYellow = 0.5
+	final var _bOrange = 0.75
+	final var _bGreen = 1.0
+	final var _bBlue = 1.25
+	final var _bBrown = 1.75
+	final var _bBlack = 2.25
+	
+	init(whiteBoard whb: Double, yellowBoard ylb: Double, orangeBoard ogb: Double, greenBoard grb: Double, blueBoard blb: Double, brownBoard brb: Double, blackBoard bkb: Double, tool tl: String) {
+		whiteBoard = whb
+		yellowBoard = ylb
+		orangeBoard = ogb
+		greenBoard = grb
+		blueBoard = blb
+		brownBoard = brb
+		blackBoard = bkb
+		date = Date()
+		tool = tl
+	}
+	
+	init() {
+		whiteBoard = 0.0
+		yellowBoard = 0.0
+		orangeBoard = 0.0
+		greenBoard = 0.0
+		blueBoard = 0.0
+		brownBoard = 0.0
+		blackBoard = 0.0
+		date = Date()
+		tool = ""
+	}
+	
+	
+	/// Returns the wood board equivalent
+	func getBoardValue() -> Double {
+		var total = 0.0
+		
+		total += (whiteBoard * _bWhite)
+		total += (yellowBoard * _bYellow)
+		total += (orangeBoard * _bOrange)
+		total += (greenBoard * _bGreen)
+		total += (blueBoard * _bBlue)
+		total += (brownBoard * _bBrown)
+		total += (blackBoard * _bBlack)
+		
+		
+		return total
+	}
+	
+	
+}
--- BoardCounter/ViewController.swift
+++ BoardCounter/ViewController.swift
@@ -7,14 +7,176 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+	@IBOutlet weak var whiteBoardTextField: UITextField!
+	@IBOutlet weak var yellowBoardTextField: UITextField!
+	@IBOutlet weak var orangeBoardTextField: UITextField!
+	@IBOutlet weak var greenBoardTextField: UITextField!
+	@IBOutlet weak var blueBoardTextField: UITextField!
+	@IBOutlet weak var brownBoardTextField: UITextField!
+	@IBOutlet weak var blackBoardTextField: UITextField!
+	@IBOutlet weak var whiteStepper: UIStepper!
+	@IBOutlet weak var yellowStepper: UIStepper!
+	@IBOutlet weak var orangeStepper: UIStepper!
+	@IBOutlet weak var greenStepper: UIStepper!
+	@IBOutlet weak var blueStepper: UIStepper!
+	@IBOutlet weak var brownStepper: UIStepper!
+	@IBOutlet weak var blackStepper: UIStepper!
+	
+	@IBOutlet weak var boardsLabel: UILabel!
+	@IBOutlet weak var boardWordLabel: UILabel!
+	@IBOutlet weak var toolPicker: UIPickerView!
+
+	
+	var currentBoard: Board
+	let appDelegate = UIApplication.shared.delegate as! AppDelegate
+	var context: NSManagedObjectContext!
+	
+	
+	required init?(coder aDecoder: NSCoder) {
+		currentBoard = Board()	
+		super.init(coder: aDecoder)
+	}
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	
+		context = appDelegate.persistentContainer.viewContext
+		toolPicker.dataSource = self
+		toolPicker.delegate = self
+		var requestReturn: [BoardData]
+		let request : NSFetchRequest<BoardData> = BoardData.fetchRequest()
+		do {
+			try requestReturn = context.fetch(request)
+			
+			currentBoard.tool = requestReturn.last!.tool!
+			toolPicker.selectRow(currentBoard.tools.firstIndex(of: currentBoard.tool)!, inComponent: 0, animated: true)
+			
+			currentBoard.date = requestReturn.last!.date!
+			
+			currentBoard.whiteBoard = requestReturn.last!.white
+			whiteBoardTextField.text = String(Int(currentBoard.whiteBoard))
+			whiteStepper.value = currentBoard.whiteBoard
+			
+			currentBoard.yellowBoard = requestReturn.last!.yellow
+			yellowBoardTextField.text = String(Int(currentBoard.yellowBoard))
+			yellowStepper.value = currentBoard.yellowBoard
+			
+			currentBoard.orangeBoard = requestReturn.last!.orange
+			orangeBoardTextField.text = String(Int(currentBoard.orangeBoard))
+			orangeStepper.value = currentBoard.orangeBoard
+			
+			currentBoard.greenBoard = requestReturn.last!.green
+			greenBoardTextField.text = String(Int(currentBoard.greenBoard))
+			greenStepper.value = currentBoard.greenBoard
+			
+			currentBoard.blueBoard = requestReturn.last!.blue
+			blueBoardTextField.text = String(Int(currentBoard.blueBoard))
+			blueStepper.value = currentBoard.blueBoard
+			
+			currentBoard.brownBoard = requestReturn.last!.brown
+			brownBoardTextField.text = String(Int(currentBoard.brownBoard))
+			brownStepper.value = currentBoard.brownBoard
+			
+			currentBoard.blackBoard = requestReturn.last!.black
+			blackBoardTextField.text = String(Int(currentBoard.blackBoard))
+			blackStepper.value = currentBoard.blackBoard
+			
+			updateBoardsLabel(currentBoard.getBoardValue())
+			
+			print("data successfully loaded")
+		}
+		catch {
+			print("failed to load data")
+		}
+		
+		
 	}
 
+	@IBAction func whiteBoardTextFieldStepperUp(_ sender: UIStepper) {
+		whiteBoardTextField.text = String(Int(sender.value))
+		currentBoard.whiteBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func yellowBoardTextFieldStepperUp(_ sender: UIStepper) {
+		yellowBoardTextField.text = String(Int(sender.value))
+		currentBoard.yellowBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func orangeBoardTextFieldStepperUp(_ sender: UIStepper) {
+		orangeBoardTextField.text = String(Int(sender.value))
+		currentBoard.orangeBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func greenBoardTextFieldStepperUp(_ sender: UIStepper) {
+		greenBoardTextField.text = String(Int(sender.value))
+		currentBoard.greenBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func blueBoardTextFieldStepperUp(_ sender: UIStepper) {
+		blueBoardTextField.text = String(Int(sender.value))
+		currentBoard.blueBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func brownBoardTextFieldStepperUp(_ sender: UIStepper) {
+		brownBoardTextField.text = String(Int(sender.value))
+		currentBoard.brownBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	@IBAction func blackBoardTextFieldStepperUp(_ sender: UIStepper) {
+		blackBoardTextField.text = String(Int(sender.value))
+		currentBoard.blackBoard = sender.value
+		updateBoardsLabel(currentBoard.getBoardValue())
+	}
+	
+	func updateBoardsLabel(_ value: Double) -> Void {
+		boardsLabel.text = String(value)
+		if value == 1 {
+			boardWordLabel.text = "board"
+		}
+		else {
+			boardWordLabel.text = "boards"
+		}
+	}
+	
+	@IBAction func saveBoardData()
+	{
+		let saveData = BoardData(context: context)
+		saveData.date = currentBoard.date
+		saveData.white = currentBoard.whiteBoard
+		saveData.yellow = currentBoard.yellowBoard
+		saveData.orange = currentBoard.orangeBoard
+		saveData.green = currentBoard.greenBoard
+		saveData.blue = currentBoard.blueBoard
+		saveData.brown = currentBoard.brownBoard
+		saveData.black = currentBoard.blackBoard
+		saveData.tool = currentBoard.tool
+		
+		do {
+			try context.save()
+			print("data saved")
+		}
+		catch {
+			print("failed to save data")
+		}
+	}
+	
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
 
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return currentBoard.tools.count
+	}
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+	return currentBoard.tools[row]
+	}
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		currentBoard.tool = currentBoard.tools[row]
+	}
 }
-
